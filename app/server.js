9209d22c0deb42dbdaaceff8ed7141b443019290
Shaun Fitzsimons <87677173+sfitz42@users.noreply.github.com>
2024-08-12 11:23:47 +0100

    Add project endpoints (#3)
    * Add project endpoints
    
    * Fix POST /project returning array
    
    * Fix linting

--- app/server.js
+++ app/server.js
@@ -8,7 +8,8 @@ const server = Hapi.server({
 
 const routes = [].concat(
   require('./routes/healthy'),
-  require('./routes/healthz')
+  require('./routes/healthz'),
+  require('./routes/projects')
 )
 
 server.route(routes)
